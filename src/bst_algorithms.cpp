df5f7545749593a47b3ddb3f9caff1ab28acf958
Ramil Safin <RamilSafNab1996@gmail.com>
2022-03-18 22:08:16 +0300

    Update bst_algorithms.cpp

--- src/bst_algorithms.cpp
+++ src/bst_algorithms.cpp
@@ -5,16 +5,8 @@
 namespace classwork {
 
   int compute_tree_height(Node* node) {
-    if (node == nullptr) {
-      return 0;
-    }
-
-    // поиск высоты левого и правого поддеревьев
-    int left_subtree_height = compute_tree_height(node->left);
-    int right_subtree_height = compute_tree_height(node->right);
-
-    // высота = максимальная высота среди поддеревьев + 1 (корневой узел)
-    return std::max(left_subtree_height, right_subtree_height) + 1;
+    // Write your code here ...
+    return 0;
   }
 
   int compute_tree_size(Node* node) {
